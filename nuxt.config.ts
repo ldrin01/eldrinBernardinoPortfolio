22e6d54f4bf508f5bfa1c2208252f711339eae67
Eldrin Bernardino <eldrinbernardino@Eldrins-MacBook-Air.local>
2025-07-12 00:06:15 +0800

    feat: Initial Nuxt 3 project setup with SSG config

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  compatibilityDate: '2025-05-15',
+
+  devtools: { enabled: true },
+  ssr: false, // Disable Server-Side Rendering
+  app: {
+    baseURL: '/eldrinBernardinoPortfolio/', // IMPORTANT: Replace 'my-nuxt-project' with your repository name. This is crucial for GitHub Pages.
+    buildAssetsDir: 'assets' // This is the default, but good to be explicit
+  },
+})
